72479853321bf3f538186451f4f450a4d21f5a43
Mojahedsuliman <mojahedmahjub@gmail.com>
2023-09-13 12:42:21 -0700

    my funuction putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * main - this is project function
+ * Return: ret 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
+	return (0);
+}
