2c2ccbcc48ae85bd78ba2e519ce0294f79fa3dba
Mojahedsuliman <mojahedmahjub@gmail.com>
2023-09-28 14:07:16 -0700

    print string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_new_recursion - print a string
+ * @s: string that will print
+ */
+void _print_rev_recursion(char *s);
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
