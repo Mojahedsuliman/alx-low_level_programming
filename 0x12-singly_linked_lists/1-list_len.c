3943ca616d51c8b3096e0f96755034d9fa4bf09a
Mojahedsuliman <mojahedmahjub@gmail.com>
2023-10-20 12:43:33 -0700

    the second task

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,20 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * list_len - length of linked lists
- * @h: first node pointer
- *
- * Return: list size
- */
+*list_len - returns the number of elements in a linked list_t list.
+*@h: pointer to list_t
+*
+*Return: return h number of elements
+*/
 size_t list_len(const list_t *h)
 {
 	size_t i = 0;
 
 	while (h)
 	{
-		 h = h->next;
-		 i++;
-	 }
-	 return (i);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,9 +23,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-int _strlen(char *s);
 size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
+
 
 #endif
