444879f5c54a3d10aa71a901fa88d207cf9b5387
Mojahedsuliman <mojahedmahjub@gmail.com>
2023-10-27 05:36:24 -0700

    the fifth task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdlib>
 
 /**
  * clear_bit - a function that sets the value of a bit to 0 at a given index.
  * @n: the number to delete
  * @index: the index of the bit
  *
- * Returns: 1 if it worked, or -1 if an error occurred
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
