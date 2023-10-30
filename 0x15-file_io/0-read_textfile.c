a14230fc2d26be15aa384a8c67077f2f748485de
Mojahedsuliman <mojahedmahjub@gmail.com>
2023-10-30 13:14:32 -0700

    the first task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file and prints it to the POSIX standard out
+ * @filename: name of the file will read
+ * @letters: number of letters to be read
+ *
+ * Return: actual number of bytes read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t nr, nw;
+	char *buf;
+	int fd;
+
+	if (filename == NULL)
+	return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	return (0);
+	while ((nr = read(fd, buf, letters)) != 0)
+	{
+		nw = write(STDOUT_FILENO, buf, nr);
+		if (nw == -1)
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (nr);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN.H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
