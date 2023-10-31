e30358c55b76999759cf8207b83d9b945a221508
Mojahedsuliman <mojahedmahjub@gmail.com>
2023-10-31 11:28:28 -0700

    the first task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,11 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
