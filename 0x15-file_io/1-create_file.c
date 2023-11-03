03ae64275132a3b6bacb3e194f9f09c0a6532ccb
Mojahedsuliman <mojahedmahjub@gmail.com>
2023-11-03 06:46:15 -0700

    the second task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,16 +21,13 @@ int create_file(const char *filename, char *text_content)
 
 		if (text_content)
 		{
-			for (n = 0; text_content[n]; n++)
-			{
-					w = write(fd, text_content, n);
+			for (w = 0; text_content[w]; w++)
+				;
 
-				if (w == -1)
+			n = write(fd, text_content, w);
+			if (n != w)
 				return (-1);
-			}
 		}
-
-		close(fd)
-
+		close(fd);
 		return (1);
 }
