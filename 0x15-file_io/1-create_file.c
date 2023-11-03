f9c682fa3d756cc4610a106971e005f2c6f8c31f
Mojahedsuliman <mojahedmahjub@gmail.com>
2023-11-03 07:24:21 -0700

    the task two

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,8 @@ int create_file(const char *filename, char *text_content)
 		if (fd == -1)
 		return (-1);
 
-		if (text_content)
-			text_content = NULL;
+		if (text_content == NULL)
+			text_content = "";
 
 		for (n = 0; text_content[n]; n++)
 			;
