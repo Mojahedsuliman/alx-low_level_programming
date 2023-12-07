258ce64aa14d2ffa287ffd6334d3d3b5c4cfeb2c
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-07 14:27:08 -0800

    the first

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
+
 /*
  * print_dlistint - prints all the elements of a dlistint_t list.
  * @h: pointer to the head of the list.
@@ -13,7 +13,9 @@ size_t print_dlistint(const dlistint_t *h);
 	if (h == NULL)
 		return (total);
 
-	while (h != NULL)
+	while (h->prev != NULL)
+		h = h->prev;
+	while(h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
