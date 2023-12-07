47e584acfb542fb78bc0f0b36559a0397669f5ff
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-07 15:03:11 -0800

    am foul

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,11 @@ size_t print_dlistint(const dlistint_t *h);
 {
 	int total = 0;
 
-	if (h == NULL)
-		return (total);
-
-	while (h->prev != NULL)
-		h = h->prev;
-	while(h != NULL)
+	if (!h)
+	{
+		return (0);
+	}
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
