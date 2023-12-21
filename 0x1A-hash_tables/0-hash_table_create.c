1d7bfe5cbf973127fb4cd96ae21cb07682ba5cfa
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-21 11:49:32 -0800

    no longer

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
  * hash_table_create - Creates a hash table
  * @size: The size of array
  *
- * Return: The pointer to the hash table that created
+ * Return: a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -12,22 +12,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
-
 	if (ht == NULL)
 		return (NULL);
-
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
-
 	return (ht);
-	prinf("\n");
 }
