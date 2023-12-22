1ad461526cc8cfd228425d1f2b68d83655e72c04
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-22 11:26:30 -0800

    therefore

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,22 +1,18 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: pointer to the hash table
+ * add_node - adds a new node to the hash table
  * @key: the key to add
- * @value: the value associated with the key (can be an empty string)
+ * @value: the value associated with the key
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_table_t *add_node(const char *key, const char *value)
 {
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
-
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
 	if (new_node == NULL)
+
 		return (0);
 
 	new_node->key = strdup(key);
@@ -26,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	new_node->value = (value != NULL) ? strdup(value) : NULL;
+	new_node->value = (value != NULL) ? strdup(value) : "";
 	if (value != NULL && new_node->value == NULL)
 	{
 		free(new_node->key);
@@ -36,7 +32,41 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to the hash table
+ * @key: the key to add or update
+ * @value: the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *node = ht->array[index];
+
+	if (value == NULL)
+		value = "";
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			if (node->value == NULL)
+				return (0);
+			return (1);
+		}
+		node = node->next;
+	}
+
+	return (add_node_to_table(ht, index, key, value));
 }
 
