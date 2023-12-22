a92996600d2b971361f7b1636c6c158a060d4be8
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-22 09:32:15 -0800

    correct definatlly

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,11 @@
 #include "hash_tables.h"
 
- /**
-  * key_index - function that gives you the index of a key.
-  * @key: key
-  * @size: size of the array of the hash table
-  *
-  * Return: the index at which should be stored in the array of the hash table
-  */
+/**
+ * key_index - function that gives you the index of a key.
+ * @key: key
+ * @size: size of the array of the hash table
+ * Return: index at which should be stored in the array of the hash table
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
