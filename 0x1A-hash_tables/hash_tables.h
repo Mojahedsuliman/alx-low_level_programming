e8e3c79d22f26ade6fe9bb502c1974f2bafdc463
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-23 11:07:53 -0800

    tyuyr

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,7 +40,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
+void hash_table_print(const hash_table_t *ht);
 
 
 #endif
