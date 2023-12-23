da18b57ba04b29c49380c34e2f686ab1a303b7f2
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-23 11:12:12 -0800

    tyuyr

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,7 +13,7 @@ void hash_table_print(const hash_table_t *ht)
 	char printed = 0;
 
 	if (ht == NULL || ht->array == NULL)
-		return (NULL);
+		return;
 	printf("{");
 
 	for (i = 0; i < ht->size; i++)
