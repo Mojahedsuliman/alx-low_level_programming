b62b8f3b20fdebcdd262704edc80e170db00d6c1
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-23 11:07:01 -0800

    therep

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table in order
+ * @ht: hash table to print
+ *
+ * Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	int printed = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return (NULL);
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (printed)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			printed = (1);
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
