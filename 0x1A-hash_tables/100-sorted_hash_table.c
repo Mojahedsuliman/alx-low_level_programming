7cc2cf0659bbc278878e10d2c084ce97ec7eb2cd
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-24 07:16:35 -0800

    tyui

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -35,30 +35,5 @@ shash_table_t *shash_table_create(unsigned long int size)
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
-	if (ht == NULL || key == NULL || value == NULL)
-		return (0);
-
-	unsigned long int index = key_index((unsigned char *)key, ht->size);
-
-	shash_node_t *new_node = malloc(sizeof(shash_node_t));
-
-	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
-
-	if (ht->shead == NULL)
-	{
-		ht->shead = new_node;
-		ht->stail = new_node;
-	}
-	else
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-
-		return (1);
-	}
+	return (1);
 }
