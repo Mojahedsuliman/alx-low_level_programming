568122cf2b7ec1e162cff381d7cb380239b833fc
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-24 04:22:55 -0800

    joker

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,26 +11,23 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *current, temp;
 	unsigned long int i;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+	{
 		return;
+	}
 	for (i = 0; i < ht->size; i++)
 	{
-		current = ht->array[i];
+		*current = ht->array[i];
 		while (current != NULL)
 		{
 			temp = current;
 			current = current->next;
 
-			if (temp->key != NULL)
-			{
-				free(temp->key);
-				temp->key = NULL;
-			}
-			if (temp->value != NULL)
-			{
-				free(temp->value);
-				temp->value = NULL;
-			}
+			free(temp->key);
+			temp->key = NULL;
+
+			free(temp->value);
+			temp->value = NULL;
 
 			free(temp);
 			temp = NULL;
@@ -40,6 +37,7 @@ void hash_table_delete(hash_table_t *ht)
 
 	free(ht->array);
 	ht->array = NULL;
+
 	free(ht);
 	ht = NULL;
 }
