08496cdb8ba46378caeab2791de33332e5472020
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-24 04:38:51 -0800

    joker

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,40 +4,29 @@
  * hash_table_delete - a function that deletes a hash table
  * @ht: hash table to delete
  *
- * Return: void
+ * Return: void 
 */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *current, temp;
+	hash_node_t *node;
 	unsigned long int i;
 
-	if (ht == NULL || ht->array == NULL || ht->size == 0)
-	{
+	if (ht == NULL || hr->array == NULL || ht->size == 0)
 		return;
-	}
+
 	for (i = 0; i < ht->size; i++)
 	{
-		*current = ht->array[i];
-		while (current != NULL)
+		while (ht->array[i] != NULL)
 		{
-			temp = current;
-			current = current->next;
-
-			free(temp->key);
-			temp->key = NULL;
-
-			free(temp->value);
-			temp->value = NULL;
-
-			free(temp);
-			temp = NULL;
+			node = ht->array[i];
+			ht->array[i] = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
 		}
-		ht->array[i] = NULL;
 	}
-
 	free(ht->array);
-	ht->array = NULL;
-
+	node = NULL;
+	ht->size = 0;
 	free(ht);
-	ht = NULL;
 }
