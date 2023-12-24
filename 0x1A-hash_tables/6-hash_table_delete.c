1ac5f32e78988b4f62e94413b7d2a802a3afe842
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2023-12-24 04:10:34 -0800

    joker

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - a function that deletes a hash table
+ * @ht: hash table to delete
+ *
+ * Return: void
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *current, temp;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+
+			if (temp->key != NULL)
+			{
+				free(temp->key);
+				temp->key = NULL;
+			}
+			if (temp->value != NULL)
+			{
+				free(temp->value);
+				temp->value = NULL;
+			}
+
+			free(temp);
+			temp = NULL;
+		}
+		ht->array[i] = NULL;
+	}
+
+	free(ht->array);
+	ht->array = NULL;
+	free(ht);
+	ht = NULL;
+}
